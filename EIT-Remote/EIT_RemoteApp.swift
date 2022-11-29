af5a33fbd2e8083319193e9020a06344854f6a70
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-11-29 12:45:21 +0300

    Initial Commit

---
+++ EIT-Remote/EIT_RemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  EIT_RemoteApp.swift
+//  EIT-Remote
+//
+//  Created by Danil Peregorodiev on 29.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct EIT_RemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
