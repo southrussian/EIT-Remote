18770431580fab2ba007a05887f8e618ece132f6
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2023-05-10 18:37:43 +0300

    Shitty code another time

--- EIT-Remote/View/ImageView.swift
+++ EIT-Remote/View/ImageView.swift
@@ -1,16 +1,10 @@
-//
-//  ImageView.swift
-//  EIT-Remote
-//
-//  Created by Danil Peregorodiev on 07.04.2023.
-//
-
 import SwiftUI
 
 struct BMPView: View {
     @State private var bmpData: Data = Data()
     @State private var ipAddress: String = ""
     @State private var portNumber: String = ""
+    @State private var showAlert: Bool = false
     
     var body: some View {
         VStack {
@@ -23,7 +17,6 @@ struct BMPView: View {
                 Text("Порт:")
                 TextField("Введите номер порта", text: $portNumber)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .keyboardType(.numberPad)
             }
             Button("Соединение") {
                 startReceiving()
@@ -40,6 +33,13 @@ struct BMPView: View {
             }
         }
         .padding()
+        .alert(isPresented: $showAlert) {
+            Alert(
+                title: Text("Ошибка подключения"),
+                message: Text("Не удалось установить соединение в течение 10 секунд."),
+                dismissButton: .default(Text("ОК"))
+            )
+        }
     }
     
     private func startReceiving() {
@@ -71,6 +71,7 @@ struct BMPView: View {
         var isFirst: Bool = true
         var headerData: Data = Data()
         var bytesReadTotal = 0
+        var timeoutCounter = 0
         
         while true {
             var buffer = [UInt8](repeating: 0, count: 1024)
@@ -92,86 +93,30 @@ struct BMPView: View {
                     print(headerData)
 
                     // Check if header is valid BMP header
-                    if headerData.count == 54 && headerData[0] == 0x42 && headerData[1] == 0x4D {
-                        print(headerData.count)
-                        // BMP header is valid, create UIImage
-                        //DispatchQueue.main.async {
-                        //self.bmpData = headerData + self.bmpData[0...] // Remove header from data
-                        print(self.bmpData)
+                    if headerData.count == 54
+                        && headerData[0] == 0x42 && headerData[1] == 0x4D {
+                                                print(headerData.count)
+                                                // BMP header is valid, create UIImage
+                                                //DispatchQueue.main.async {
+                                                //self.bmpData = headerData + self.bmpData[0...] // Remove header from data
+                                                print(self.bmpData)
 
-                        //}
-                    }
-                }
-            } else {
-                print("Stream closed")
-                return
-            }
-        }
-    }
-
-
-    
-//    private func startReceiving() {
-//        guard let serverAddress = ipAddress as CFString?, let port = UInt32(portNumber) else {
-//            print("Invalid server address or port")
-//            return
-//        }
-//
-//        var inputStream: InputStream?
-//        var outputStream: OutputStream?
-//        Stream.getStreamsToHost(withName: serverAddress as String, port: Int(port), inputStream: &inputStream, outputStream: &outputStream)
-//
-//        guard let stream = inputStream else {
-//            print("Failed to create stream")
-//            return
-//        }
-//
-//        stream.open()
-//        let data = Data([1])
-//        outputStream?.open()
-//        let bytesWritten = outputStream?.write([UInt8](data), maxLength: 4)
-////        print(bytesWritten)
-//        if bytesWritten == -1 {
-//            print("Error sending data")
-//        } else {
-//            print("Data sent successfully")
-//        }
-//        var isFirst: Bool = true
-//        var headerData: Data = Data()
-//        while true {
-//            var buffer = [UInt8](repeating: 0, count: 1024)
-//            let bytesRead = stream.read(&buffer, maxLength: buffer.count)
-//            print(bytesRead)
-//
-//            if bytesRead > 0 {
-//                if isFirst == true{
-//                    headerData.append(contentsOf: buffer[8..<62])
-//                    self.bmpData.append(contentsOf: buffer[8..<bytesRead])
-//                    isFirst=false
-//                }
-//                else{
-//                    self.bmpData.append(contentsOf: buffer[0..<bytesRead])
-//                }
-//
-//                    print(headerData)
-//
-//                    // Check if header is valid BMP header
-//                    if headerData.count == 54 && headerData[0] == 0x42 && headerData[1] == 0x4D {
-//                        print(headerData.count)
-//                        // BMP header is valid, create UIImage
-//                        //DispatchQueue.main.async {
-//                            //self.bmpData = headerData + self.bmpData[0...] // Remove header from data
-//                            print(self.bmpData)
-//
-//                    //}
-//                }
-//            } else {
-//                print("Stream closed")
-//                return
-//            }
-//        }
-//    }
-}
-
-
-
+                                                //}
+                                            }
+                                        }
+                                    } else if bytesRead == 0 {
+                                        print("Stream closed")
+                                        return
+                                    }
+                                    
+                                    // Increase the timeout counter
+                                    timeoutCounter += 1
+                                    
+                                    // Check if the timeout limit has been reached (10 seconds)
+                                    if timeoutCounter >= 10 {
+                                        showAlert = true
+                                        return
+                                    }
+                                }
+                            }
+                        }
