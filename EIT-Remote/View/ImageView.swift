7a18771c7245e9b6742543a52971303a9a00dcff
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2023-05-10 18:31:43 +0300

    Another boring commit

--- EIT-Remote/View/ImageView.swift
+++ EIT-Remote/View/ImageView.swift
@@ -23,6 +23,7 @@ struct BMPView: View {
                 Text("Порт:")
                 TextField("Введите номер порта", text: $portNumber)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.numberPad)
             }
             Button("Соединение") {
                 startReceiving()
@@ -46,33 +47,59 @@ struct BMPView: View {
             print("Invalid server address or port")
             return
         }
-        
+
         var inputStream: InputStream?
         var outputStream: OutputStream?
         Stream.getStreamsToHost(withName: serverAddress as String, port: Int(port), inputStream: &inputStream, outputStream: &outputStream)
-        
+
         guard let stream = inputStream else {
             print("Failed to create stream")
             return
         }
-        
+
         stream.open()
+        let data = Data([1])
+        outputStream?.open()
+        let bytesWritten = outputStream?.write([UInt8](data), maxLength: 4)
+
+        if bytesWritten == -1 {
+            print("Error sending data")
+        } else {
+            print("Data sent successfully")
+        }
+        
+        var isFirst: Bool = true
+        var headerData: Data = Data()
+        var bytesReadTotal = 0
+        
         while true {
             var buffer = [UInt8](repeating: 0, count: 1024)
             let bytesRead = stream.read(&buffer, maxLength: buffer.count)
-            
+            print(bytesRead)
+
             if bytesRead > 0 {
-                bmpData.append(contentsOf: buffer[0..<bytesRead])
-                if bmpData.count > 54 { // BMP header is 54 bytes
-                    // Extract BMP header
-                    let headerData = bmpData.subdata(in: 0..<54)
-                    
+                bytesReadTotal += bytesRead
+                if isFirst {
+                    headerData.append(contentsOf: buffer[8..<62])
+                    self.bmpData.append(contentsOf: buffer[62..<bytesRead])
+                    isFirst = false
+                } else {
+                    self.bmpData.append(contentsOf: buffer[0..<bytesRead])
+                }
+
+                if isFirst == false && bytesReadTotal == self.bmpData.count {
+                    // The first image has been fully received
+                    print(headerData)
+
                     // Check if header is valid BMP header
                     if headerData.count == 54 && headerData[0] == 0x42 && headerData[1] == 0x4D {
+                        print(headerData.count)
                         // BMP header is valid, create UIImage
-                        DispatchQueue.main.async {
-                            self.bmpData = headerData + self.bmpData[54...] // Remove header from data
-                        }
+                        //DispatchQueue.main.async {
+                        //self.bmpData = headerData + self.bmpData[0...] // Remove header from data
+                        print(self.bmpData)
+
+                        //}
                     }
                 }
             } else {
@@ -81,6 +108,69 @@ struct BMPView: View {
             }
         }
     }
+
+
+    
+//    private func startReceiving() {
+//        guard let serverAddress = ipAddress as CFString?, let port = UInt32(portNumber) else {
+//            print("Invalid server address or port")
+//            return
+//        }
+//
+//        var inputStream: InputStream?
+//        var outputStream: OutputStream?
+//        Stream.getStreamsToHost(withName: serverAddress as String, port: Int(port), inputStream: &inputStream, outputStream: &outputStream)
+//
+//        guard let stream = inputStream else {
+//            print("Failed to create stream")
+//            return
+//        }
+//
+//        stream.open()
+//        let data = Data([1])
+//        outputStream?.open()
+//        let bytesWritten = outputStream?.write([UInt8](data), maxLength: 4)
+////        print(bytesWritten)
+//        if bytesWritten == -1 {
+//            print("Error sending data")
+//        } else {
+//            print("Data sent successfully")
+//        }
+//        var isFirst: Bool = true
+//        var headerData: Data = Data()
+//        while true {
+//            var buffer = [UInt8](repeating: 0, count: 1024)
+//            let bytesRead = stream.read(&buffer, maxLength: buffer.count)
+//            print(bytesRead)
+//
+//            if bytesRead > 0 {
+//                if isFirst == true{
+//                    headerData.append(contentsOf: buffer[8..<62])
+//                    self.bmpData.append(contentsOf: buffer[8..<bytesRead])
+//                    isFirst=false
+//                }
+//                else{
+//                    self.bmpData.append(contentsOf: buffer[0..<bytesRead])
+//                }
+//
+//                    print(headerData)
+//
+//                    // Check if header is valid BMP header
+//                    if headerData.count == 54 && headerData[0] == 0x42 && headerData[1] == 0x4D {
+//                        print(headerData.count)
+//                        // BMP header is valid, create UIImage
+//                        //DispatchQueue.main.async {
+//                            //self.bmpData = headerData + self.bmpData[0...] // Remove header from data
+//                            print(self.bmpData)
+//
+//                    //}
+//                }
+//            } else {
+//                print("Stream closed")
+//                return
+//            }
+//        }
+//    }
 }
 
 
